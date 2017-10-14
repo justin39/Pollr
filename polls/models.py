981b52965d1cc3dc2a9894cf81e0cf9a9e4ce0d3
Graeme <ggmferguson@gmail.com>
2017-10-14 17:49:32 -0400

    add song model and migrate

--- polls/models.py
+++ polls/models.py
@@ -9,3 +9,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Song(models.Model):
+    sID = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    artists = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    votes = models.PositiveIntegerField(default=0)

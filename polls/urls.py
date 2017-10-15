35f5fc0a49949ac233a20cb073408f05747ec844
reece <rtr266@nyu.edu>
2017-10-14 20:46:08 -0400

    fix syntax error

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^search$', views.search, name='search')
+    url(r'^search$', views.search, name='search'),
     url(r'^voting$', views.voting, name='voting')
 ]
